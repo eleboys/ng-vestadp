79588ece7d255bd2910121813b65d75f8b41cb02
Nima Baghdadi <bg.nima@gmail.com>
2017-03-01 17:20:23 +0330

    selected date moved to ng-model

--- src/angular.vestadatepicker.js
+++ src/angular.vestadatepicker.js
@@ -9,35 +9,19 @@
         var directive = {
             link: link,
             restrict: 'A',
-            require: 'ngModel',
-            scope: {
-                vdpSelectedDate: "="
-            }
+            require: 'ngModel'
         };
         return directive;
 
         function link(scope, element, attrs, ngModel) {
-            scope.options = JSON.parse(attrs.vestadp) || {};
-            angular.extend(scope.options, {
+            var options = JSON.parse(attrs.vestadp) || {};
+            angular.extend(options, {
                 dateChanged: function (elm, date) {
-                    if (!scope.vdpSelectedDate || !scope.vdpSelectedDate.toDateString || scope.vdpSelectedDate.toDateString()!=elm.vestadp('getDate').toDateString()){
-                        scope.vdpSelectedDate = elm.vestadp('getDate');
-                    }
-                    ngModel.$setViewValue(date);
-                    ngModel.$render();
+                    ngModel.$setViewValue(elm.vestadp('getDate'));
                 }
             });
 
-            var vdp = $(element).vestadp(scope.options);
-
-            scope.$watch("vdpSelectedDate", function(){
-                if (scope.vdpSelectedDate){
-                    // if (vdp.vestadp('getDate').toDateString()!=scope.vdpSelectedDate.toDateString()){
-                        var d = scope.vdpSelectedDate;
-                        vdp.vestadp('setDate', {year:d.getFullYear(), month: d.getMonth()+1, day: d.getDate()});
-                    // }
-                }
-            });
+            var vdp = $(element).vestadp(options);
         }
     }
 
